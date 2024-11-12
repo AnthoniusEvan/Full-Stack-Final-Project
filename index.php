add7d9cd47548ae4ffb64955e1d6ad52c34960e8
Evanesce <anthoniusevan@gmail.com>
2024-11-12 16:02:39 +0700

    minor fix

--- index.php
+++ index.php
@@ -528,8 +528,8 @@ if (isset($_GET['logout'])){
                                 <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
                                     <div class="user-menu d-flex">
                                         <div class="user-name text-end me-3">
-                                            <h6 class="mb-0 text-gray-600"><?php echo $_SESSION['active_user']->name; ?></h6>
-                                            <p class="mb-0 text-sm text-gray-600"><?php echo $_SESSION['active_user']->role; ?></p>
+                                            <h6 class="mb-0 text-gray-600"><?php if(isset($_SESSION['active_user'])) echo $_SESSION['active_user']->name; ?></h6>
+                                            <p class="mb-0 text-sm text-gray-600"><?php if(isset($_SESSION['active_user'])) echo $_SESSION['active_user']->role; ?></p>
                                         </div>
                                         <div class="user-img d-flex align-items-center">
                                             <div class="avatar avatar-md">
@@ -540,9 +540,9 @@ if (isset($_GET['logout'])){
                                 </a>
                                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
                                     <li>
-                                        <h6 class="dropdown-header">Hello, <?php echo explode(' ',$_SESSION['active_user']->name)[0]; ?>!</h6>
+                                        <h6 class="dropdown-header">Hello, <?php if(isset($_SESSION['active_user'])) echo explode(' ',$_SESSION['active_user']->name)[0]; ?>!</h6>
                                     </li>
-                                    <li><a class="dropdown-item" href="index.php?page=staff_detail&mode=update&id=<?php echo $_SESSION['active_user']->id ?>"><i class="icon-mid bi bi-person me-2"></i> My
+                                    <li><a class="dropdown-item" href="index.php?page=staff_detail&mode=update&id=<?php if(isset($_SESSION['active_user'])) echo $_SESSION['active_user']->id ?>"><i class="icon-mid bi bi-person me-2"></i> My
                                             Profile</a></li>
                                     <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-gear me-2"></i>
                                             Settings</a></li>
